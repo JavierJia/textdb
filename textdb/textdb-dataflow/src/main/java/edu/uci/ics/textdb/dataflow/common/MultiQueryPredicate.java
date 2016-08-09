0f5906a078566deb4a98de273ae51d3059993f6f
zuozhiw <zuozhiw@uci.edu>
2016-08-09 14:35:44 -0700

    initial addition of multiQueryIndex and multiQueryPredicate

---
+++ textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/common/MultiQueryPredicate.java
@@ -0,0 +1,9 @@
+package edu.uci.ics.textdb.dataflow.common;
+
+public class MultiQueryPredicate {
+    
+    public MultiQueryPredicate(List<Query> queryList, List<String> queryStringList, List<Attribute> attributeList, Analyzer luceneAnalyzer) {
+        
+    }
+
+}
